00a240a1c68f4aafa73e76e677744da88c75ed63
Brandon Fox <bl.fox@live.com>
2016-01-20 14:19:11 -0800

    Basic customize funtion added

--- js/script.js
+++ js/script.js
@@ -3,34 +3,80 @@ var pearlsApp = angular.module('pearlsApp', ['ngRoute']);
 $('.modal').hide();
 
 
-pearlsApp.config(function($routeProvider, $locationProvider){
+pearlsApp.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
-      templateUrl : 'templates/home.html',
+      templateUrl: 'templates/home.html',
       controller: 'HomeCtrl'
     })
     .when('/customize', {
-      templateUrl : 'templates/customize.html'
+      templateUrl: 'templates/customize.html',
+      controller: 'CustomCtrl'
+
     })
     .when('/about', {
-      templateUrl : 'templates/about.html'
+      templateUrl: 'templates/about.html'
     })
     .when('/contact', {
-      templateUrl : 'templates/contact.html'
+      templateUrl: 'templates/contact.html'
     });
 
-    $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 }).controller('HomeCtrl', function($scope, $location) {
-    $scope.form = {
-        username: null,
-        password: null
-    };
+  $scope.form = {
+    username: null,
+    password: null
+  };
 
-    $scope.goCustom = function() {
-      $('li a').each(function(){
-        $(this).removeClass('selected');
-      });
-      $('.nav-customize').addClass('selected');
-        $location.url('/customize');
-    };
-})
+  $scope.goCustom = function() {
+    $('li a').each(function() {
+      $(this).removeClass('selected');
+    });
+    $('.nav-customize').addClass('selected');
+    $location.url('/customize');
+  };
+}).controller('CustomCtrl', function($scope) {
+  $scope.chosenRight = "1flip.png";
+  $scope.chosenLeft = "1.png";
+  $scope.radioRight = [{
+    label: "Episode 1",
+    val: "1flip.png"
+  }, {
+    label: "Episode 2",
+    val: "2flip.png"
+  },  {
+    label: "Episode 3",
+    val: "3flip.png"
+  },  {
+    label: "Episode 4",
+    val: "4flip.png"
+  },  {
+    label: "Episode 5",
+    val: "5flip.png"
+  },  {
+    label: "Episode 6",
+    val: "6flip.png"
+  }];
+  $scope.radioLeft = [{
+    label: "Episode 1",
+    val: "1.png"
+  }, {
+    label: "Episode 2",
+    val: "2.png"
+  },  {
+    label: "Episode 3",
+    val: "3.png"
+  },  {
+    label: "Episode 4",
+    val: "4.png"
+  },  {
+    label: "Episode 5",
+    val: "5.png"
+  },  {
+    label: "Episode 6",
+    val: "6.png"
+  }];
+
+
+
+});
