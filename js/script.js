0ad86d244c38b7efc5b36b7a782510e2f6afd349
Brandon Fox <bl.fox@live.com>
2016-03-25 08:37:00 -0700

    Testing error

--- js/script.js
+++ js/script.js
@@ -236,20 +236,20 @@ pearlsApp.config(function($routeProvider, $locationProvider) {
     preloader.preloadImages($scope.imageLocations).then(
       function handleResolve(imageLocations) {
         // Loading was successful.
-        $scope.isLoading = Loaded.setLoad(false);
-        $("#loading").css("display","none");
-
-        $("#content--home").fadeIn();
-
-        var mq2 = window.matchMedia("(max-width: 620px)");
-        var mq3 = window.matchMedia("(max-device-width: 620px)");
-        if (mq2.matches || mq3.matches) {
-          $(".nav__topbar").fadeIn();
-        }else {
-          $(".nav--main").fadeIn();
-        }              resize.onstart();
-
-        $scope.isSuccessful = true;
+        // $scope.isLoading = Loaded.setLoad(false);
+        // $("#loading").css("display","none");
+        //
+        // $("#content--home").fadeIn();
+        //
+        // var mq2 = window.matchMedia("(max-width: 620px)");
+        // var mq3 = window.matchMedia("(max-device-width: 620px)");
+        // if (mq2.matches || mq3.matches) {
+        //   $(".nav__topbar").fadeIn();
+        // }else {
+        //   $(".nav--main").fadeIn();
+        // }              resize.onstart();
+        //
+        // $scope.isSuccessful = true;
       },
       function handleReject(imageLocation) {
         // Loading failed on at least one image.
@@ -272,9 +272,11 @@ pearlsApp.config(function($routeProvider, $locationProvider) {
         console.info("Preload Failure");
       },
       function handleNotify(event) {
+        if (percent < 50){
         $scope.percentLoaded = event.percent;
         red.style.height = (parseInt(red.style.height, 10) - 2) + "px";
         red.style.top = (parseInt(red.style.top, 10) - 2) + "px";
+        }
       }
     );
   }
